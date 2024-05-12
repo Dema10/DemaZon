20dcdad6ff83cc296e572c61261e0328724f8b04
“Dema10 <gabrieledematteis@me.com>
2024-05-13 01:20:34 +0200

    manca un po di stile

--- Back/backOffice.js
+++ Back/backOffice.js
@@ -1,25 +1,41 @@
+// Definizione della costante url che contiene l'endpoint dell'API per i prodotti
 const url = "https://striveschool-api.herokuapp.com/api/product/";
-// chiave autorizzazione api
+
+// Definizione della costante token contenente la chiave di autorizzazione per l'API
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjNmNjQ2NDAwYzFjMjAwMTU3ZTk5ZTciLCJpYXQiOjE3MTU0MzA1MDEsImV4cCI6MTcxNjY0MDEwMX0.59cbRvlhFuyF2Rj7zYDrzhKbCILlo42wzUVaSAjNF4M";
+
+// Selezione del form HTML tramite l'ID "product-form"
 const form = document.getElementById("product-form");
+
+// Selezione del bottone di modifica HTML tramite l'ID "modify"
 const modify = document.getElementById("modify");
+
+// Selezione del bottone di aggiunta HTML tramite l'ID "add"
 const add = document.getElementById("add");
-// funzione che parte e chiama le sue funzioni all'avvio della pagina
+
+// Funzione che viene eseguita quando la finestra è completamente caricata
 window.onload = async () => {
-    // chiamo la funzione per la visualizzazione a video dei prodotti con await in modo tale che si attende il suo svolgimento
-    // prima del prossimo step
-    await showProducts(); // funzione mostra prodotti
-    getValueForm(); // recupera i dati del prodotto da modificare
+    // Chiamata alla funzione che mostra i prodotti
+    await showProducts();
+
+    // Chiamata alla funzione che recupera i dati del prodotto per la modifica
+    getValueForm();
 }
 
+// Funzione che crea un nuovo prodotto
 const createProducts = async (event) => {
+    // Impedisce il comportamento predefinito del form
     event.preventDefault();
+
+    // Recupero dei valori inseriti dall'utente nel form
     const constName = document.getElementById("name").value;
     const constdDscription = document.getElementById("description").value;
     const constBrand = document.getElementById("brand").value;
     const constImageUrl = document.getElementById("image-img").value;
     const constPrice = document.getElementById("price").value;
-
+      
+    
+    // Creazione di un nuovo oggetto prodotto
     const newProduct = {
         name: constName,
         description: constdDscription,
@@ -28,46 +44,56 @@ const createProducts = async (event) => {
         price: constPrice,
     };
 
-    const res = await fetch (url, {
+    // Chiamata all'API per aggiungere un nuovo prodotto
+    const res = await fetch(url, {
         method: "POST",
         headers: {
             "Authorization": `Bearer ${token}`,
             "content-type": "application/json",
         },
         body: JSON.stringify(newProduct),
-    })
-    
+    });
 
+    // Se la chiamata ha successo, mostra un messaggio di successo e aggiorna i prodotti visualizzati
     if (res.ok) {
         mostraSpinner();
         mostraMessaggio("Prodotto creato con successo");
         await showProducts();
-    }else {
-        mostraMessaggio("Erorre, ritenta sarai più fortunato");
+    } else {
+        // Altrimenti mostra un messaggio di errore
+        mostraMessaggio("Errore, ritenta sarai più fortunato");
     }
 };
- 
 
-// funzione per creare la card del prodotto
+// Funzione per visualizzare i prodotti
 const showProducts = async () => {
+    // Resetta il form
     form.reset();
-    const res = await fetch (url, {
+
+    // Chiamata all'API per ottenere i prodotti
+    const res = await fetch(url, {
         headers: {
             "Authorization": `Bearer ${token}`,
         }
     });
+
+    // Converte la risposta in formato JSON
     const products = await res.json();
+
+    // Selezione del contenitore dei prodotti
     const productsContainer = document.getElementById("products-list");
+
+    // Se il contenitore dei prodotti esiste
     if (productsContainer) {
+        // Popola il contenitore con le card dei prodotti
         productsContainer.innerHTML = products.map((product) => `
             <div class="col-12 d-flex justify-content-between align-items-center ">
                 <img src="${product.imageUrl}" width="80px" height="100px">
                 <div class="d-flex flex-column justify-content-center">
                     <h4>${product.name}</h4>
-                    <p>${product.description}</p> 
                     <div class="d-flex flex-row align-items-center">
                         <h6 class="mb-0 pe-1">${product.brand}</h6>
-                        <small><b>${product.price}</b></small>
+                        <small><b>${product.price},00 €</b></small>
                     </div>
                 </div>
                 <div class="d-flex flex-column justify-content-center"> 
@@ -77,31 +103,41 @@ const showProducts = async () => {
             </div>
         `).join("");
     }
+    // Nasconde lo spinner una volta caricati i prodotti
     nascondiSpinner();
 };
 
+// Funzione per ottenere i valori del prodotto selezionato per la modifica
 const getValueForm = async (idInInput) => {
+    // Se è stato passato un ID
     if (idInInput) {
-        const res = await fetch (url +idInInput, {
+        // Chiamata all'API per ottenere i dettagli del prodotto
+        const res = await fetch(url + idInInput, {
             headers: {
                 "Authorization": `Bearer ${token}`,
             }
         });
+
+        // Converte la risposta in formato JSON
         const product = await res.json();
+
+        // Popola i campi del form con i dettagli del prodotto
         document.getElementById("name").value = product.name;
         document.getElementById("description").value = product.description;
         document.getElementById("brand").value = product.brand;
         document.getElementById("image-img").value = product.imageUrl;
         document.getElementById("price").value = product.price;
         document.getElementById("id").value = product._id;
+
+        // Mostra il bottone di modifica e nasconde il bottone di aggiunta
         modify.classList.remove("d-none");
         add.classList.add("d-none");
     }
 };
 
-
-//funzione per modificare i prodotti
+// Funzione per modificare un prodotto
 const modifyProduct = async () => {
+    // Recupero dei valori dal form
     const constName = document.getElementById("name").value;
     const constdDscription = document.getElementById("description").value;
     const constBrand = document.getElementById("brand").value;
@@ -109,6 +145,7 @@ const modifyProduct = async () => {
     const constPrice = document.getElementById("price").value;
     const id = document.getElementById("id").value;
 
+    // Creazione dell'oggetto prodotto modificato
     const modifyProduct = {
         name: constName,
         description: constdDscription,
@@ -117,7 +154,8 @@ const modifyProduct = async () => {
         price: constPrice,
     };
 
-    const res = await fetch (url + id, {
+    // Chiamata all'API per modificare il prodotto
+    const res = await fetch(url + id, {
         method: "PUT",
         headers: {
             "Authorization": `Bearer ${token}`,
@@ -125,51 +163,57 @@ const modifyProduct = async () => {
         },
         body: JSON.stringify(modifyProduct),
     });
-    
 
+    // Se la chiamata ha successo, mostra un messaggio di successo e aggiorna i prodotti visualizzati
     if (res.ok) {
         mostraSpinner();
         mostraMessaggio("Prodotto aggiornato con successo");
         await showProducts();
-    }else {
-        mostraMessaggio("Erorre, ritenta sarai più fortunato");
+    } else {
+        // Altrimenti mostra un messaggio di errore
+        mostraMessaggio("Errore, ritenta sarai più fortunato");
     }
 };
 
-
+// Funzione per eliminare un prodotto
 const deleteProduct = async (id) => {
-    const res = await fetch (url + id, {
+    // Chiamata all'API per eliminare il prodotto
+    const res = await fetch(url + id, {
         method: "DELETE",
         headers: {
             "Authorization": `Bearer ${token}`,
         },
     });
 
+    // Se la chiamata ha successo, mostra un messaggio di successo e aggiorna i prodotti visualizzati
     if (res.ok) {
         mostraSpinner();
         mostraMessaggio("Prodotto eliminato con successo");
         await showProducts();
-    }else {
-        mostraMessaggio("Erorre, ritenta sarai più fortunato");
+    } else {
+        // Altrimenti mostra un messaggio di errore
+        mostraMessaggio("Errore, ritenta sarai più fortunato");
     }
-}; 
+};
 
-
-
+// Funzione per mostrare lo spinner
 function mostraSpinner() {
     document.getElementById("spinner").style.display = "block";
 };
-  
+
+// Funzione per nascondere lo spinner
 function nascondiSpinner() {
     document.getElementById("spinner").style.display = "none";
 };
 
+// Funzione per mostrare un messaggio
 function mostraMessaggio(messaggio) {
     const messageBox = document.getElementById("message-box");
     messageBox.textContent = messaggio;
     messageBox.style.display = "block";
 
+    // Il messaggio scompare dopo 3 secondi
     setTimeout(() => {
         messageBox.style.display = "none";
-    }, 3000); // Il messaggio scompare dopo 3 secondi
+    }, 3000);
 };
---
+++ front/frontPage.js
@@ -0,0 +1,47 @@
+// Definizione della costante url che contiene l'endpoint dell'API per i prodotti
+const url = "https://striveschool-api.herokuapp.com/api/product/";
+
+// Definizione della costante token contenente la chiave di autorizzazione per l'API
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjNmNjQ2NDAwYzFjMjAwMTU3ZTk5ZTciLCJpYXQiOjE3MTU0MzA1MDEsImV4cCI6MTcxNjY0MDEwMX0.59cbRvlhFuyF2Rj7zYDrzhKbCILlo42wzUVaSAjNF4M";
+
+// Funzione che viene eseguita quando la finestra è completamente caricata
+window.onload = async () => {
+    // Chiamata alla funzione che mostra i prodotti
+    await showProducts();
+}
+
+
+const showProducts = async () => {
+    // Chiamata all'API per ottenere i prodotti
+    const res = await fetch(url, {
+        headers: {
+            "Authorization": `Bearer ${token}`,
+        }
+    });
+
+    // Converte la risposta in formato JSON
+    const products = await res.json();
+
+    // Selezione del contenitore dei prodotti
+    const productsContainer = document.getElementById("products-list");
+
+    // Se il contenitore dei prodotti esiste
+    if (productsContainer) {
+        // Popola il contenitore con le card dei prodotti
+        productsContainer.innerHTML = products.map((product) => `
+            <div class="col-12 box">
+                <img src="${product.imageUrl}" width="200px" height="220px">
+                <div class="mt-1">
+                    <h4>${product.name}</h4>
+                    <div class="mt-1">
+                        <h6 class="mb-0 pe-1">${product.brand}</h6>
+                        <small><b>${product.price},00 €</b></small>
+                    </div>
+                </div>
+                <div class="mt-2"> 
+                    <button class="submitbtn detail mb-1"><a class="text-decoration-none text-white" href="/detail/detail.html?id=${product._id}">Dettaglio</a></button>
+                </div>
+            </div>
+        `).join("");
+    }
+};
